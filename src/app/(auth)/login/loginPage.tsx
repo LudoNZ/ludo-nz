6f0c26b9bc7afcc06eb8a12f223f1059ec67bd2b
Ludo <ludobourneville@gmail.com>
2025-04-18 21:07:03 +1200

    login with google

--- src/app/(auth)/login/loginPage.tsx
+++ src/app/(auth)/login/loginPage.tsx
@@ -1,7 +1,24 @@
 "use client"
 
+import Button from "@/components/button/button"
+import { signInWithPopup } from "firebase/auth"
+import { GoogleAuthProvider } from "firebase/auth"
+import { auth } from "../../../../firebase/client"
+
 const LoginPage = () => {
-  return <div>login Page</div>
+  return (
+    <div>
+      <h2>login Page</h2>
+      <Button
+        children="Conitnue with Google"
+        size="large"
+        onClick={() => {
+          const provider = new GoogleAuthProvider()
+          signInWithPopup(auth, provider)
+        }}
+      />
+    </div>
+  )
 }
 
 export default LoginPage
